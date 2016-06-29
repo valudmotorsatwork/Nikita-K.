e49f0ef8ab6baebd340e79d742cae9daebee5892
nkayasth <nkayasth@VSPL1115CPU78.co.valudconsulting.com>
2016-06-29 16:26:38 +0530

    Assignment5_28/6/16

---
+++ Assign_28_5/src/com/String/Application.java
@@ -0,0 +1,17 @@
+//5.Write an application that reads several lines of text and prints a table indicating the number of occurrences of each different word in the text. The first version of your program should include the words in the table in the same order in which they appear in the text.
+//number of one-letter words, two-letter words, three-letter words, etc. appearing in the text. 
+package com.String;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception
+	{
+		
+		System.out.println("Enter the lines...");
+		Logic obj=new Logic();
+		obj.check();
+
+	}
+
+}
+
---
+++ Assign_28_5/src/com/String/Logic.java
@@ -0,0 +1,47 @@
+package com.String;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Logic
+{
+	 public void check()
+	   {
+	     Scanner scr=new Scanner(System.in);
+	       String s=null;
+	        
+	      s=scr.nextLine();
+	      System.out.println("Your String Is.. " +s);
+	    
+	    try
+	       {
+	      System.out.printf( "%-15s %15s %n","Words Length","Occurances");
+	      String line[]= s.split(" ");
+	      ArrayList< String > list = new ArrayList< String >();
+
+	       list.addAll( Arrays.asList(line) );
+
+	       int cnt = 0;
+
+	       for( int i = 0; i < list.size(); i++ )
+	        {
+	        for( int j = 0; j < list.size(); j++ )
+	           {
+	            if( list.get( i ).equals(list.get( j ) ) )
+	               cnt++;
+	             if( list.get( i ).equals( list.get( j ) ) && cnt > 1 )
+	              list.remove( j );                      
+	           
+	           }
+	        System.out.printf("%-15s %15s %n",( list.get( i )), cnt );
+	        cnt = 0;
+	               
+	            }
+	          System.out.println();
+	          }
+	         catch(Exception e)
+	           {
+	            e.printStackTrace();}
+}
+}
