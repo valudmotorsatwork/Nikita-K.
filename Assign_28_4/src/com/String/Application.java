81e0bd7516a95ed0a6343ab96510452dd52954d3
nkayasth <nkayasth@VSPL1115CPU78.co.valudconsulting.com>
2016-06-29 16:25:07 +0530

    Assignment4_28/6/16

---
+++ Assign_28_4/src/com/String/Application.java
@@ -0,0 +1,17 @@
+//4.Write an application that reads several lines of text and prints a table indicating the 
+//number of one-letter words, two-letter words, three-letter words, etc. appearing in the text. 
+package com.String;
+
+public class Application {
+
+	public static void main(String[] args) throws Exception
+	{
+		
+		System.out.println("Enter the sequence of Strings...");
+		Logic obj=new Logic();
+		obj.check();
+
+	}
+
+}
+
